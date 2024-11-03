8bd1c3bccf91bccd702ebda2632a20e2c5f5e117
bencmbit98 <104804447+bencmbit98@users.noreply.github.com>
2024-11-04 07:41:06 +0800

    Update 2_Methodology.py

--- pages/2_Methodology.py
+++ pages/2_Methodology.py
@@ -1,4 +1,18 @@
 import streamlit as st
 
-st.header("Methodogy Used")
-st.write("RAG")
+st.header("Methodology for RAG Chatbot Development")
+st.write("Prototype by Benedict Fernandez (TP)")
+
+st.markdown(''':green[This chatbot prototype uses a Retrieval-Augmented Generation (RAG) model \
+to generate responses based on content from three external websites. \
+By combining retrieval and generation, the model provides accurate, \
+contextually relevant answers to user queries. \
+Below is the detailed methodology for the chatbot’s development, structured in five sequential stages: \
+Document Loading, Splitting & Chunking, Storage, Retrieval, and Output.]''')
+
+st.markdown(''':orange[For more content details and information, please visit \
+[TP Special Educational Needs Support](https://www.tp.edu.sg/life-at-tp/special-educational-needs-sen-support.html") \
+and [Enabling Guide by SG Enable](https://www.enablingguide.sg/).]''')
+
+st.markdown(''':orange[This prototype is developed by Benedict Fernandez. \
+Please feel free to give your comments, feedback & suggestion by reaching me at <bencmbit@gmail.com>]''')
