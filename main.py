f1c05097ebffb4dcb209241881623735d822fbb3
bencmbit98 <104804447+bencmbit98@users.noreply.github.com>
2024-11-03 19:43:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -157,8 +157,8 @@ st.set_page_config(
 st.title("Support SEN Student")
 st.write("in Temasek Polytechnic")
 # Check if the password is correct.  
-if not check_password():  
-    st.stop()
+# if not check_password():  
+#    st.stop()
 
 final_text = RAG_Load()
 splitted_documents = RAG_SplittingChunking(final_text)
