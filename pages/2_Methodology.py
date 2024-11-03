156fa989aeea3398f38bca0178ba0fcee14b40c4
bencmbit98 <104804447+bencmbit98@users.noreply.github.com>
2024-11-03 21:00:17 +0800

    Add files via upload

---
+++ pages/2_Methodology.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.header("Methodogy Used")
+st.write("RAG")
---
+++ pages/3_About_Us.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+# region <--------- Streamlit App Configuration --------->
+st.set_page_config(
+    layout="centered",
+    page_title="ABC Capstone Project"
+)
+# endregion <--------- Streamlit App Configuration --------->
+
+st.title("ABC Capstone Project")
+st.write("This is a Streamlit App that demonstrates how to use the OpenAI API to generate text completions.")
+
+with st.expander("How to use this App"):
+    st.write("1. Enter your prompt in the text area.")
+    st.write("2. Click the 'Submit' button.")
+    st.write("3. The app will generate a text completion based on your prompt.")
+
+with st.expander("How to use this App"):
+    st.write("IMPORTANT NOTICE: This web application is a prototype developed for educational purposes only. The information provided here is NOT intended for real-world usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters.")
+    st.write("Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output.")
+    st.write("Always consult with qualified professionals for accurate and personalized advice.")
