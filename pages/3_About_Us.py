4e39d1b0870177ed81ebe4e41f3db936f11bff9d
bencmbit98 <104804447+bencmbit98@users.noreply.github.com>
2024-11-03 23:08:19 +0800

    Update 3_About_Us.py

--- pages/3_About_Us.py
+++ pages/3_About_Us.py
@@ -14,12 +14,8 @@ st.header("# About US")
 st.title("Methodology")
 st.write("This is a Streamlit App that demonstrates how to use the OpenAI API to generate text completions.")
 
-with st.expander("How to use this App")
-    st.write("1. Enter your question in the text area.")
-    st.write("2. Click the 'Send' button.")
-    st.write("3. The app will generate a text completion based on your prompt.")
 
-st.markdown("**Important Notice**"):
+st.write("**Important Notice**"):
 st.write("This web application is a prototype developed for educational purposes only. The information provided here is NOT intended for real-world usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters.")
 st.write("Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output.")
 st.write("Always consult with qualified professionals for accurate and personalized advice.")
