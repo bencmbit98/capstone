8caad054e513627ecfcaec04004f616729af8c52
bencmbit98 <104804447+bencmbit98@users.noreply.github.com>
2024-11-03 20:55:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -168,3 +168,7 @@ if form.form_submit_button("Send"):
     response = qa_chain.invoke(user_prompt)
     answer = response["result"]
     st.write(answer)
+    with st.expander("Important Disclaimer"):
+      st.write("IMPORTANT NOTICE: This web application is a prototype developed for educational purposes only. The information provided here is NOT intended for real-world usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters.")
+      st.write("Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output.")
+      st.write("Always consult with qualified professionals for accurate and personalized advice.")
